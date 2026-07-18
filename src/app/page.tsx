e9095a1f13ee119bcdcfc6a20a3be12a89a51d3b
Tamanna431 <tamannashuchi06@gmail.com>
2026-07-19 02:10:10 +0600

    feat: add environmental impact charts using Recharts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -276,7 +276,7 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Section 7: FAQ */}
+      {/* Sections 7: FAQ */}
       <section className="py-20 md:py-28 bg-oat">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
